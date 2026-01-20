ee335ee1634a70319649a62051a7ba1d5962e844
Rizky Ananda <Rananda381@gmail.com>
2026-01-20 20:37:47 +0700

    Update main.js dan pelengkap App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 import landingpage from "./components/landingpage.vue"
 import loginapp from "./components/loginform.vue"
 import dashboard from "./components/dashboard.vue"
@@ -7,14 +6,10 @@ import dashboard from "./components/dashboard.vue"
 
 <template>
   <div>
-    <HelloWorld msg="Vite + Vue" /> 
+    <HelloWorld msg="Portofolio Saya" />
 
     <landingpage />
     <loginapp />
     <dashboard />
   </div>
 </template>
-
-<style scoped>
-
-</style>
