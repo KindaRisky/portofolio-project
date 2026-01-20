81566b1635d151f35362decf11e23fce4c8ae09f
Rizky Ananda <Rananda381@gmail.com>
2026-01-20 20:22:19 +0700

    Update main.js dan pelengkap App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+// 1. Bagian Import (Sudah benar)
 import HelloWorld from './components/HelloWorld.vue'
 import landingpage from "./components/landingpage.vue"
 import loginapp from "./components/loginform.vue"
@@ -7,27 +8,14 @@ import dashboard from "./components/dashboard.vue"
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <HelloWorld msg="Vite + Vue" /> 
+
+    <landingpage />
+    <loginapp />
+    <dashboard />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+/* Tambahkan CSS jika perlu */
 </style>
