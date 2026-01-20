742dcea48db090d405a28f18b4b8ba90d609e138
Rizky Ananda <Rananda381@gmail.com>
2026-01-20 20:08:18 +0700

    Update tampilan utama App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import landingpage from "./components/landingpage.vue"
+import loginapp from "./components/loginform.vue"
+import dashboard from "./components/dashboard.vue"
 </script>
 
 <template>
