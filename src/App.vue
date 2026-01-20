ddc4d961e83a73bca306bc20e38ac3218f164c5e
Rizky Ananda <Rananda381@gmail.com>
2026-01-20 20:42:49 +0700

    Update main.js dan pelengkap App.vue

--- src/App.vue
+++ src/App.vue
@@ -13,3 +13,20 @@ import dashboard from "./components/dashboard.vue"
     <dashboard />
   </div>
 </template>
+
+<style>
+/* Reset margin dan padding bawaan browser */
+body, html {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  overflow-x: hidden; /* Mencegah scroll ke samping */
+}
+
+#app {
+  max-width: 100% !important; /* Menghilangkan batasan lebar 1280px */
+  margin: 0 !important;
+  padding: 0 !important;
+  width: 100vw;
+}
+</style>
