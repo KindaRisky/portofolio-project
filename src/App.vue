80caeec6513fd9b302e0bb1f249e6ffe7491e183
Rizky Ananda <Rananda381@gmail.com>
2026-01-20 20:20:26 +0700

    Update main.js dan pelengkap App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import HelloWorld from './components/HelloWorld.vue'
 import landingpage from "./components/landingpage.vue"
 import loginapp from "./components/loginform.vue"
 import dashboard from "./components/dashboard.vue"
